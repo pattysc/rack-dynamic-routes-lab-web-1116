b66974d46950bdaf970e4993c2b365fe02d67ddb
pattysc <pattys@users.noreply.github.com>
2016-12-14 15:17:42 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,27 @@
+# Your application should only accept the /items/<ITEM NAME> route. Everything else should 404
+# If a user requests /items/<Item Name> it should return the price of that item
+# IF a user requests an item that you don't have, then return a 400 and an error message
+
+class Application
+  def call(env)
+    request = Rack::Request.new(env)
+    response = Rack::Response.new
+
+    if request.path.match(/items/)
+      groceryitem = request.path.split("/items/").last
+
+      gi = Item.find(groceryitem)
+
+      if gi
+        response.write "#{gi.price}"
+      else
+        response.write "Item not found"
+        response.status = 400
+      end
+    else
+      response.write "Route not found"
+      response.status = 404
+    end
+    response.finish
+  end
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,20 @@
 class Item
-  attr_accessor :name, :price
+  attr_accessor :name, :price, :items
+
+  @@items = []
 
   def initialize(name,price)
     @name = name
     @price = price
+    @@items << self
   end
+
+  def self.find(name)
+    @@items.each do |item|
+      if name == item.name
+        return item
+      end
+    end
+  end
+  
 end
